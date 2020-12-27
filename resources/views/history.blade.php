a464072145bd88a13f79c5ed52b4d1c73a67a274
beaventura <53884647+beaventura@users.noreply.github.com>
2020-12-27 22:12:41 +0800

    Frontend for History and some minor changes

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,19 +1,36 @@
 @extends('layouts\app')
 @section('content')
-<table>
-  <thead>
-    <th>Order ID</th>
-    <th>Food Name</th>
-    <th>Price</th>
-  </thead>
-  <tbody>
-    @foreach($history as $item)
-    <tr>
-    <td>{{$item['id']}}</td>
-    <td>{{$item['foodName']}}</td>
-    <td>{{$item['price']}}</td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
+  <div class="history-container" id="top"></div>
+
+  <div class="container">
+        <div class="row separator-top center">
+            <div class="col offset-md">
+                <h1 class="product-title">Purchase History</h1>
+                <p class="div-separator"></p>
+                <h6>Recall your past transaction</h6>
+            </div>
+        </div>
+    </div>
+
+  <div class="container">
+    <div class="row center">
+      <table class>
+      <thead>
+          <th class="center">Order No</th>
+          <th class="center" style="width: 700px;">Food Name</th>
+          <th class="center" style="width: 150px;">Price</th>
+        </thead>
+        <tbody>
+          @foreach($history as $item)
+          <tr>
+          <td class="center">{{$item['id']}}</td>
+          <td>{{$item['foodName']}}</td>
+          <td class="center">₱ {{$item['price']}}.00</td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 @endsection
